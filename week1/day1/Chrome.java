3a1cc6ef000ae8d1606d822734d5f8f2c7b51d87
gdeebika <gdeebika@gmail.com>
2025-05-24 20:31:13 -0700

    Add files via upload
    Week 1 day 1

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version = 91.0f;
+				System.out.println("Version Value is " + version);
+				
+		String Developer = "Google";
+				System.out.println("Developer Value is " + Developer);
+				
+		boolean isBeta = false;
+				System.out.println("isBeta Value is " + isBeta);
+				
+		int releaseYear = 2008;
+				System.out.println("releaseYear Value is " + releaseYear);
+				
+		char shortCut = 'C';
+				System.out.println("shortCut Value is " + shortCut);
+
+
+	}
+
+}
---
+++ week1/day1/Firstprogram_001.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Firstprogram_001 {
+
+	//type main then ctrl+space -> select from suggestion 
+	public static void main(String[] args) {
+		
+		System.out.println("Hi");
+
+	}
+
+}
